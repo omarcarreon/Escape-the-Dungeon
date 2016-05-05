5f76a78e258404347ff361e7d29f712660f9e852
MegaTuks <a_marcel93@hotmail.com>
2016-05-05 18:51:48 -0500

    adds menu

---
+++ EscapeTheDungeon/Assets/Scripts/Menu/MenuScene.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour {
+
+	public GameObject insPanel;
+	public GameObject credPanel;
+	void Start () 
+	{
+		insPanel.SetActive (false);
+		credPanel.SetActive (false);
+	}
+
+	public void lookCredits () {
+		credPanel.SetActive (true);
+	}
+
+	public void closeCredits () {
+		credPanel.SetActive (false);
+	}
+	public void LookInstructions ()
+	{
+		insPanel.SetActive (true);
+	}
+	public void CloseIns ()
+	{
+		insPanel.SetActive (false);
+	}
+	public void StartGame () 
+	{
+		SceneManager.LoadScene ("Nivel");
+	}
+	public void endGame () 
+	{
+		Application.Quit ();
+	}
+}

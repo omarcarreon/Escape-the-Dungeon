6b0fef0750d307fe019e43e2fc20adf3e8e43107
Leonardo Gutiérrez <a01089731@gmail.com>
2016-05-06 11:45:36 -0500

    Sangre

--- EscapeTheDungeon/Assets/Scripts/Player/Player.cs
+++ EscapeTheDungeon/Assets/Scripts/Player/Player.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
+
+	private const int MAX_VIDA = 5000;
+	public int vida = MAX_VIDA;
+	public Text txtVida;
+	private int score = 0;
+	public Text txtScore;
 
 	// Use this for initialization
 	void Start () {
 	
+		txtVida.text = "Vida: " + ((vida * 100) / MAX_VIDA) + "%";
+		txtScore.text = "Puntos: " + score;
+
 	}
 	
 	// Update is called once per frame
@@ -17,9 +27,52 @@ public class Player : MonoBehaviour {
 
 		if(col.gameObject.name == "Heart"){
 			Destroy(col.gameObject);
+			vida += 250;
+			if (vida > MAX_VIDA) {
+				vida = MAX_VIDA;
+			}
+			txtVida.text = "Vida: " + ((vida * 100) / MAX_VIDA) + "%";
 		}
 
 
 
 	}
 }
+
+/*
+ * using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MsgSaliendoJuego : MonoBehaviour {
+
+	public Text mensaje;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter(Collider col){
+	
+		if (col.gameObject.name == "RigidBodyFPSController") {
+			mensaje.text = "Estás saliendo del área de juego";
+	
+		}
+	}
+
+	void onTriggerExit(Collider col){
+
+		if (col.gameObject.name == "RigidBodyFPSController") {
+			mensaje.text = "";
+
+		}
+	}
+}
+
+ * */
